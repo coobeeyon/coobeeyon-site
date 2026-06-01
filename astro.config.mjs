82f23bca80f87ffe3d33d68d6c443944549a9e31
Michael Daum <mike.daum@coobeeyon.net>
2026-06-01 14:55:33 -0400

    Configure Netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: process.env.SITE_URL ?? 'https://coobeeyon.net',
+	site: process.env.SITE_URL ?? process.env.URL ?? 'https://coobeeyon.net',
 	integrations: [mdx(), sitemap()],
 });
